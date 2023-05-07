e99ecb09d2fb85a4c13f5a64e865c9015f73b14d
mixberryparfait <test@test.com>
2023-05-07 14:43:08 +0900

    manifest v3

--- copy.js
+++ copy.js
@@ -1,34 +1,35 @@
-var debug = true;
+const voice = false;
 
-//localStorage.clear();
-
-if (debug) console.log("execute copy.js");
+console.log("execute copy.js");
 
 // バトルページのみ
 if (location.href.indexOf('#raid') >= 0) {
 
-  // ポップアップが出現したときに呼ばれる
-  // ID をクリップボードにコピーするイベントを登録してコピーイベントを呼び出す
-
   window.onload = function() {
+
+    // ポップアップが出現したときに呼ばれる
+    // ID をクリップボードにコピーするイベントを登録してコピーイベントを呼び出す
     const observer = new MutationObserver((mutations) => {
       var elems = document.getElementsByClassName('prt-battle-id');
       if (elems.length <= 0) {
         return;
       }
       const id = elems[0].textContent.trim();
-      document.addEventListener('copy', function(e) {
+
+      const copy = e => {
         e.preventDefault();
         e.clipboardData.setData('text/plain', id);
-        console.log('copy ' + id)
-      });
+        console.log('copy ' + id);
+        if(voice) speak(id.split('').join(' '));
+      };
+
+      document.addEventListener('copy', copy);
       document.execCommand("copy");
+      document.removeEventListener('copy', copy);
     });
 
-    // pop 要素が生成されるまでループ
     const wait_generate_pop = () => {
       const target = document.getElementById('pop');
-      console.log(target);
       if(target) {
         // pop 要素の中を見張る
         const config = { 
@@ -39,10 +40,23 @@ if (location.href.indexOf('#raid') >= 0) {
         };
         observer.observe(target, config);
       }
+      // pop 要素が生成されるまでループ
+      // リロードのたびに呼ばれるので間隔を短くしすぎるとリロード処理が重くなる
+      // 長くしすぎると最速で救援窓を開いたときに動かない
+      // 自分の環境では 1.5 秒ぐらいで生成される
       else
-        setTimeout(wait_generate_pop, 100);
+        setTimeout(wait_generate_pop, 1500);
     };
 
     wait_generate_pop();
   }
+
+  const speak = (text) => {
+    const speak   = new SpeechSynthesisUtterance();
+    speak.text = text;
+    speak.rate  = 2; // 読み上げ速度 0.1-10 初期値:1 (倍速なら2, 半分の倍速なら0.5, )
+    speak.pitch = 3;
+    speak.volume = 0.5;
+    speechSynthesis.speak(speak);
+  } 
 }
